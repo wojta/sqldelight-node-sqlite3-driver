defd3d7b8e08c0485f3feeae1f0aaa4a7bd1700b
Vojtěch Sázel <wojta@users.noreply.github.com>
2026-09-25 00:02:32 +0200

    remove deprecated executeSuspendingAsList

--- src/jsMain/kotlin/cz/sazel/sqldelight/node/sqlite3/utils.kt
+++ src/jsMain/kotlin/cz/sazel/sqldelight/node/sqlite3/utils.kt
@@ -4,19 +4,6 @@ import app.cash.sqldelight.Query
 import app.cash.sqldelight.db.QueryResult
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.toList
-
-/**
- * Workaround suspending method to use with SQLite3 async driver.
- * Use this instead of non-async method [Query.executeAsList].
- * @return The result set of the underlying SQL statement as a list of RowType.
- */
-@Deprecated(
-    "Use awaitAsList() instead, will be removed in 0.7",
-    ReplaceWith("awaitAsList()", "app.cash.sqldelight.async.coroutines.awaitAsList")
-)
-suspend fun <T : Any> Query<T>.executeSuspendingAsList(): List<T> =
-    executeAsFlow().toList()
 
 /**
  * Workaround suspending method to use with SQLite3 async driver.
